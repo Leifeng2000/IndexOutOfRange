e757db4a0ad6a2122e755404e1386efec1f11484
Leifeng2000 <leifeng7112@gmail.com>
2024-05-04 11:52:46 +0700

    U

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<char> characters = new List<char>();
+        characters.AddRange(new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+        try
+        {
+            List<char> characters2 = new List<char>();
+            characters2.AddRange(new Char[] { 'a', 'b', 'c', 'd', 'e', 'f' });
+            int value = characters2[6];
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            System.Console.WriteLine(ex);
+        }
+    }
+}
